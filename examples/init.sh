5cb56698104bb968dbc8f3a93c410bee1d68fc64
Sudhi Herle <sudhi@herle.net>
2017-07-07 13:48:31 +0900

    Added more docs and removed unconditional mount of /dev

--- examples/init.sh
+++ examples/init.sh
@@ -2,35 +2,45 @@
 
 # post-exec script run in the parent's context. Use this to setup
 # network plumbing for the cloned namespace and mounting a devtmpfs
-# etc.
-#
-# Finally, exec the real-init of the chroot.
+# etc. Finally, exec the real-init of the chroot.
 #
 # Environment vars set conditionally:
 #
-#   CLONE_NEWUSER
-#   CLONE_NEWNET
+#   CLONE_NEWUSER   -- set if 'ns' is invoked with '-u'
+#   CLONE_NEWNET    -- set if 'ns' is invoked with '-n'
+#   DEBUG           -- set if the 'ns' utility is built in debug mode
+#
+# By the time this script is invoked, 'ns' has already done the
+# following:
+#  - parent has:
+#     * setup uid/gid mapping (if needed)
+#     * setup network plumbing
+#  - child has:
+#  -  * new root is mounted and the process has pivoted its root
+#  -  * mounted /proc
 #
 
-mkdir -p /proc /dev /sys          || exit 1
-
-mount -t proc proc /proc          || exit 2
-mount -t devtmpfs devtmpfs /dev   || exit 3
-mount -t sysfs sysfs /sys         || exit 4
+# Looks like we can't mount these two if we are running under a
+# user-namespace (at least on kernel <= 4.9)
+if [  -n "$CLONE_NEWUSER" ]; then
+    mount -t devtmpfs devtmpfs /dev   || exit 3
+    mount -t sysfs sysfs /sys         || exit 4
+fi
 
 
 # The IP address here mirrors the setup in pre.sh.
 # For production use, rely on DHCP instead of hard-coding these.
-ip addr add 10.99.88.2/24 dev eth0 || exit 5
-ip link set up dev eth0            || exit 6
+ip addr add 10.99.88.2/24 dev eth0  || exit 5
+ip link set up dev eth0             || exit 6
+ip route add default via 10.99.88.1 || exit 7
 
 # Environment var set by the caller
 if [ -n "$DEBUG" ]; then
-    exec /bin/sh                   || exit 7
+    exec /bin/sh                   || exit 8
 else
     for f in /sbin /bin; do
         if [ -f $f/init ]; then
-            exec $f/init           || exit 7
+            exec $f/init           || exit 9
         fi
     done
     echo "$0: No working init found!" 1>&2
--- examples/pre.sh
+++ examples/pre.sh
@@ -18,6 +18,8 @@ if [ -z "$CLONE_NEWNET" ]; then
     exit 0
 fi
 
+# Network name of 'eth0' should be the same one used in the child
+# namespace. 'veth0' is the interface name in the host (parent namespace).
 ip link add name veth0 type veth peer eth0 netns $Child || exit 1
 ip addr add 10.99.88.1/24 dev veth0                     || exit 2
 ip link set up dev veth0                                || exit 3
